6a560c72b9d66b443d740d6127c1f95b643ed38c
Calen Ray <Calen.w.ray@gmail.com>
2021-04-28 18:24:46 -0700

    added stick figure turtle per assignment

---
+++ Code/Calen/python/1-turtle.py
@@ -0,0 +1,62 @@
+"""
+Author: Calen Ray
+Date: 4/28/21
+project: Turtle
+"""
+
+from turtle import *
+
+right(55)
+
+forward(100)
+
+back(100)
+
+right (65)
+
+forward(100)
+
+back(100)
+
+left(210)
+
+forward(175)
+
+left(150)
+
+forward(50)
+
+back(50)
+
+right(300)
+
+forward(50)
+
+back(50)
+
+left(150)
+
+forward(50)
+
+left(90)
+
+forward(25)
+
+right(90)
+
+forward(50)
+
+right(90)
+
+forward(50)
+
+right(90)
+
+forward(50)
+
+right(90)
+
+forward(25)
+
+
+done()
