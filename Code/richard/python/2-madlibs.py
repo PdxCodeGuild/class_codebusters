4542ed676b3002a2dc0fdd488e57ad9a78b78f26
Richard Lee <lee.richardm@gmail.com>
2021-04-28 21:08:47 -0700

    completed madlibs version 2

--- Code/richard/python/2-madlibs.py
+++ Code/richard/python/2-madlibs.py
@@ -6,6 +6,8 @@
 # Ask the user for each word you'll put in your Mad Lib.
 # Use string concatenation to combine with user input with other strings to form the Mad Lib.
 # Display the Mad Lib to the user.
+import random
+'''
 sun = input("Please enter a noun: ")
 dreams = input("Please enter something you would chase: ")
 regret = input("Please enter a verb: ")
@@ -15,4 +17,14 @@ When the {sun} rises
 I wake up and chase my {dreams}
 I won't {regret} when the sun sets
 'Cause I live my life like I'm a {beast}
+""")
+'''
+# Version 2 Make a functional solution that utilizes lists
+userInputs = input("Please enter four nouns seperated by commas: ")
+splitInput = userInputs.split(",")
+print(f"""
+When the {splitInput[random.randint(0,3)]} rises
+I wake up and chase my {splitInput[random.randint(0,3)]}
+I won't regret when the {splitInput[random.randint(0,3)]} sets
+'Cause I live my life like I'm a {splitInput[random.randint(0,3)]}
 """)
