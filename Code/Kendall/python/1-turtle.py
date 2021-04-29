78d00d22dc2a31641e06904ed51a2b097796c896
Kendall <kendallmackey25@gmail.com>
2021-04-28 18:30:09 -0700

    Added Lab 01

---
+++ Code/Kendall/python/1-turtle.py
@@ -0,0 +1,38 @@
+from turtle import *
+
+# Square Head
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+# Upper Body
+forward(50)
+right(90)
+forward(100)
+right(90)
+
+# Arms
+forward(100)
+right(180)
+forward(200)
+right(180)
+forward(100)
+
+# Lower Body
+left(90)
+forward(100)
+
+# Legs
+right(45)
+forward(100)
+right(180)
+forward(100)
+right(90)
+forward(100)
+
+done()
