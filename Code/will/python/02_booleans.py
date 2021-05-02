8b974731c13ef7387bcd08561d4be2c0aae88cd4
lu-will <williamtlu@outlook.com>
2021-05-02 14:25:46 -0700

    added booleans lab

--- Code/will/python/02_booleans.py
+++ Code/will/python/02_booleans.py
@@ -30,12 +30,11 @@ def test_go_hiking():
 # Write a function that returns True if the number is a double digit
 
 def double_digit(num):
+    num = abs(num)
     length = len(str(num))
     if length == 2:
         return True
-    elif
-        return True
-    else
+    else:
         return False
 
 
@@ -46,16 +45,15 @@ def test_double_digit():
     assert double_digit(-56) == True
 
 
-'''
 # Opposite
 # Write a function that takes two integers, `a` and `b`, and returns `True` if one is positive and the other is negative,
 # and return `False` otherwise.
 
 def opposite(a, b):
     if a > 0 and b < 0:
-        print('True')
+        return True
     else:
-        print('False')
+        return False
 
 
 def test_opposite():
@@ -69,10 +67,12 @@ def test_opposite():
 
 
 def near_100(num):
-    if 10 < num < 100:
-        print(True)
+    if 90 < num <= 99:
+        return True
+    elif 101 < num <= 110:
+        return True
     else:
-        print('False')
+        return False
 
 
 def test_near_100():
@@ -81,19 +81,15 @@ def test_near_100():
     assert near_100(105) == True
     assert near_100(115) == False
 
-
 # Maximum of Three
 # Write a function that returns the maximum of 3 parameters.
 
 
 def maximum_of_three(a, b, c):
-    if a, b, c == 'True':
-        print(max(a, b, c))
+    numbers = [a, b, c]
+    return(max(numbers))
 
 
 def test_maximum_of_three():
     assert maximum_of_three(5, 6, 2) == 6
     assert maximum_of_three(-4, 3, 10) == 10
-
-'''
-
