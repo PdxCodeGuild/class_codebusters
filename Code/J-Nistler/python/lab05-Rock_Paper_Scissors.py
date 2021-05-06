2ced66c0171981fff04ad522fb9560b5e24fd18b
J-Nistler <jmnistl@protonmail.com>
2021-05-06 00:04:11 -0400

    add v3 of lab05

--- Code/J-Nistler/python/lab05-Rock_Paper_Scissors.py
+++ Code/J-Nistler/python/lab05-Rock_Paper_Scissors.py
@@ -40,11 +40,12 @@ Rock, paper, scissors, lizard, Spock!
 '''
 Version 1: Complete
 Version 2: Complete
-Version 3: Under Development
+Version 3: Complete
 
 Future Improvements:
 
 - Fix Version two to not use a nested if statement
+- Add better loser message based on outcome
 
 '''
 
@@ -52,7 +53,12 @@ Future Improvements:
 # Import Dependencies
 #-----------------------------------------------#
 import random
-choices = ["Rock", "Paper", "Scissors"]
+choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+win_rock = ["Lizard", "Scissors"]
+win_paper = ["Spock", "Rock"]
+win_sci = ["Lizard", "Paper"]
+win_liz = ["Spock", "Paper"]
+win_spock = ["Scissors", "Rock"]
 
 #-----------------------------------------------#
 # Compare Choices
@@ -61,8 +67,16 @@ choices = ["Rock", "Paper", "Scissors"]
 def throw_choice(user_input, comp_input):
     if user_input == comp_input:
         return "It was a Tie"
-    elif (user_input == "Paper" and comp_input == "Rock") or (user_input == "Scissors" and comp_input == "Paper") or (user_input == "Rock" and comp_input == "Scissors"):
-        return "You win!!"
+    elif user_input == "Rock" and comp_input in win_rock:
+        return "You win! Rock crushes scissors and lizards"
+    elif user_input == "Paper" and comp_input in win_paper:
+        return "You win! Paper disproves spock and covers rock"
+    elif user_input == "Scissors" and comp_input in win_sci:
+        return "You win! Scissors cuts paper and lizards"
+    elif user_input == "Lizard" and comp_input in win_liz:
+        return "You win! Lizards eat paper and poison Spock"
+    elif user_input == "Spock" and comp_input in win_spock:
+        return "You win! Spock vaporizes rock and smashes scissors"
     else:
         return "You lose. Womp womp."
 
@@ -72,7 +86,6 @@ def end_message (user, comp, result):
     You threw {user}, the computer threw {comp}
     {result}
     ------------------------------------------------
-
     '''
 
 #-----------------------------------------------#
@@ -82,11 +95,11 @@ def end_message (user, comp, result):
 while True: 
     # Prompt user for input
     user_choice = input (f'''
-    ------------------------------------------------
+    -------------------------------------------------------------------------
     You have been chosen.
 
-    Make your selection: Rock, Paper, Scissors, or Exit: 
-    ------------------------------------------------
+    Make your selection: Rock, Paper, Scissors, Lizard, Spockor, or Exit: 
+    -------------------------------------------------------------------------
     ''').title()
 
     # Validate input and start game
@@ -100,15 +113,18 @@ while True:
         
         # Ask the player if they want to play again
         again_choice = input(f'''
-        
-        Would you like to play again? Yes or No.
-        ''').title()
+    Would you like to play again? Yes or No.
+    ''').title()
 
+        # If the user wants to play again, loop back
         if again_choice == "Yes":
             continue
+        # If the user does not want to play anymore, break the loop
         else:
             break
     elif user_choice == "Exit":
         break
+    # If the user put in invalid data, ask for it again
     else:
+        print("Please input a valid selection")
         continue
