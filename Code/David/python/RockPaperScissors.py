9cfb4fef83e3f3eb056875130cd12f4ba9ef9905
DavidReichley <davidreichley6@gmail.com>
2021-05-05 20:08:45 -0700

    add paper rock scissor

--- Code/David/python/RockPaperScissors.py
+++ Code/David/python/RockPaperScissors.py
@@ -1,11 +1,21 @@
 import random
-user = input("Choose rock, paper, or scissors: ")
-computer = ["rock", "paper", "scissors"]
-'rock' > "scissors" > 'paper'
-choice = random.choice(computer)
-print('I chose ' + choice)
+a = True
+while a:
+    user = input("Choose rock, paper, or scissors: ")
 
-if user > choice:
-    print('You win')
-if choice > user:
-    print('I win')
+    computer = ["rock", "paper", "scissors"]  # choices for random
+    'rock' > "scissors" > 'paper' > 'rock'  # establish game rules
+    choice = random.choice(computer)  # get computers choice
+    print('I chose ' + choice)  # lets user know what computer chose
+
+    if user > choice:  # let user know who won
+        print('Crap, You win')  # sore loser
+    if choice > user:
+        print('Haha, I win')  # jerk
+    if choice == user:
+        print('Great minds think alike.')  # ties happen
+    next = input('would you like to play again, yes or no? ')
+
+    if next == 'no':
+        a = False  # ends game
+        print('Id be scared to play me too, goodbye')  # still a jerk
