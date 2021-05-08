9e6f506a19fc42b7223c5d4edc8b68cf913e6917
techdevjorge <jsanmartijr@gmail.com>
2021-05-07 19:00:44 -0700

    finished 06_loops_practice.py

--- Code/jorge/python/06_loops_practice.py
+++ Code/jorge/python/06_loops_practice.py
@@ -15,3 +15,32 @@ def double_numbers(nums):
 
 def test_double_numbers():
     assert double_numbers([1, 2, 3]) == [2, 4, 6]
+
+# Stars
+# Write a function that takes an integer and returns that number of asterisks in a string
+
+
+def stars(n):
+    return '*' * n
+
+
+def test_stars():
+    assert stars(1) == '*'
+    assert stars(2) == '**'
+    assert stars(3) == '***'
+    assert stars(4) == '****'
+
+# Extract Less Than Ten
+# Write a function to move all the elements of a list with value less than 10 to a new list and return it.
+
+
+def extract_less_than_ten(nums):
+    new_list = []
+    for n in nums:
+        if n < 10:
+            new_list.append(n)
+    return new_list
+
+
+def test_extract_less_than_ten():
+    assert extract_less_than_ten([2, 8, 12, 18]) == [2, 8]
