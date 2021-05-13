3e68dd1e38bfb607020b165b5b9a7935dad5478f
Anthony <anthonyt.wallace@icloud.com>
2021-05-12 20:58:42 -0700

    Updated comments in bubblesort.py

--- Code/anthony/python/bubblesort.py
+++ Code/anthony/python/bubblesort.py
@@ -10,17 +10,17 @@ sort_me = []
 for x in range(n):
     sort_me.append(random.randint(0, 99))
 
-# Keep track of if swapped
-# Checks each index
 swapped = True
 while swapped:
+    # Keep track of if swapped
     swapped = False
+    # Checks each index
     for i in range(len(sort_me)-1):
+        # if current > next index : swap elements
         if sort_me[i] > sort_me[i+1]:
             sort_me[i], sort_me[i+1] = sort_me[i+1], sort_me[i]
             swapped = True
+    # If swapped is still false, list is sorted
 
 end_time = time.time()
 print(f"bubble sort took {end_time - start_time} seconds")
-# if current > next index : swap elements
-# If swapped is still false, list is sorted
