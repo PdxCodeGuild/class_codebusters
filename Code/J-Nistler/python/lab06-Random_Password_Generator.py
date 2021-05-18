7a93e328fa42bdcd8d0fd60ae744fe89af329d8c
J-Nistler <jmnistl@protonmail.com>
2021-05-17 23:26:06 -0400

    add implementation for v1 and v2

--- Code/J-Nistler/python/lab06-Random_Password_Generator.py
+++ Code/J-Nistler/python/lab06-Random_Password_Generator.py
@@ -29,39 +29,129 @@ to convert the string to a list, random.shuffle(password_list)
 to shuffle it, and then ''.join(password_list) to turn it back 
 into a string.
 '''
+
 #-----------------------------------------------#
 # Developer Notes
 #-----------------------------------------------#
 '''
-Version 1: 
-Version 2: 
+Version 1: Complete
+Version 2: Complete
 
 Future Improvements:
-
 - 
 
 '''
-
 #-----------------------------------------------#
 # Import Dependencies
 #-----------------------------------------------#
 
+# import necessary dependencies
+import random
 import string
 
-# Define banks of character types in which to choose
-punc_bank = string.punctuation
-upper_bank = string.ascii_uppercase
+#-----------------------------------------------#
+# Create Neccesary Objects
+#-----------------------------------------------#
+
+# Creates character banks for each character type
 lower_bank = string.ascii_lowercase
-dig_bank = string.digits
+upper_bank = string.ascii_uppercase
+digit_bank = string.digits
+symbol_bank = string.punctuation
 
-# Solicit user input
-input_pass_len = input("How many characters would you like your password? ")
-input_num_upper = input("How many uppercase letters? ")
-input_num_lower = input("How many lowercase letters? ")
-input_num_dig = input("How many digits? ")
+# create a string in which to store the generated password
+password = ""
 
+#-----------------------------------------------#
+# Input from User
+#-----------------------------------------------#
+# LENGTH OF PASSWORD
+while True:
+# recieves input from user on password length
+    num_upper = input("How many upper-case letters? ")
+# convert user input to int
+    try:
+        num_upper = int(num_upper)
+        break
+    except TypeError:
+        print("Please input a valid integer")
+        continue
 
+# NUMBER OF LETTERS
+while True:
+    # prompts user for number of letters
+    num_lower = input("How many lower-case letters? ")
+    try:
+        # convert user input to int
+        num_lower = int(num_lower)
+        break
+    except TypeError:
+        print("Please input a valid integer")
+        continue
 
-# for i in range(len(input_pass_len)):
+# NUMBER OF DIGITS
+while True:
+    # prompts user for number of digits
+    num_digit = input("How many digits? ")
+    try:
+        # convert user input to int
+        num_digit = int(num_digit)
+        break
+    except TypeError:
+        print("Please input a valid integer")
+        continue
 
+# NUMBER OF SYMBOLS
+while True:
+    # prompts user for number of digits
+    num_sym = input("How many symbols? ")
+    try:
+        # convert user input to int
+        num_sym = int(num_sym)
+        break
+    except TypeError:
+        print("Please input a valid integer")
+        continue
 
+#-----------------------------------------------#
+# Add characters to Password
+#-----------------------------------------------#
+
+# defines a function that takes in the character source and number of 
+# characters to generate, then returns those characters in a string.
+def pass_gen (char_source, num):
+    # create counter
+    counter = 0
+    pass_seg = ""
+    # adds specified number of character type to the password
+    while counter < num:
+        # adds random character to the password segment from the character bank
+        pass_seg += random.choice(char_source)
+        # marks the counter
+        counter += 1
+    return pass_seg
+
+#-----------------------------------------------#
+# Construct Password
+#-----------------------------------------------#
+
+# calls on the pass_gen function to generate the appropriate character type
+# and number of symbols as specified by the user. Concatenates into one string
+password = pass_gen(upper_bank, num_upper) + pass_gen(lower_bank, num_lower) + pass_gen(digit_bank, num_digit) + pass_gen(symbol_bank, num_sym)
+# convert password string to list
+password = list(password)
+# jumble password characters
+random.shuffle(password)
+# re-assemble characters into string
+password = ''.join(password)
+
+#-----------------------------------------------#
+# Print Password
+#-----------------------------------------------#
+
+# prints generated password for the user
+print(f'''
+--------------------
+Generated password: {password}
+--------------------
+''')
