3acc148370faef544243893b61105ff523969cfd
smdavis14 <smdavis14@yahoo.com>
2021-05-21 09:37:39 -0700

    adding 4 versions 10-unit_converter

---
+++ Code/steve/python/10-unit_converter.py
@@ -0,0 +1,40 @@
+"""
+Unit Converter
+
+This lab will involve writing a program that allows the user to convert a number between units.
+Version 1
+
+Ask the user for the number of feet, and print out the equivalent distance in meters. 
+Hint: 1 ft is 0.3048 m. So we can get the output in meters by multiplying the input distance by 0.3048. 
+Below is some sample input/output.
+
+> what is the distance in feet? 12
+> 12 ft is 3.6576 m
+"""
+
+# dictionary to hold the relationship of meters to another measure like feet (ft) or miles (mi)
+distance_dict = {
+    'ft': 0.3048
+}
+
+
+# def to get and validate user input
+def get_distance_input():
+    while True:
+        user_distance = input('Enter the distance you would like to convert: ')
+        try:
+            user_distance = float(user_distance)
+            break
+        except ValueError:
+            print('Invalid entry, you must enter a number.')
+    return user_distance
+
+
+# calculate and return the converted distance
+def convert_distance():
+    distance = get_distance_input()
+    return f"{distance} ft is {distance_dict['ft'] * distance:.4f} meters."
+
+
+if __name__ == '__main__':
+    print(convert_distance())
