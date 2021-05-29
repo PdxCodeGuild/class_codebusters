0c587faf810c4eab8c190b7ef053b7b046d336e1
J-Nistler <jmnistl@protonmail.com>
2021-05-28 21:37:39 -0400

    refine naming convention for python files

--- Code/J-Nistler/python/lab07-Peaks_and_Valleys.py
+++ Code/J-Nistler/python/lab07-Peaks_and_Valleys.py
@@ -2,7 +2,7 @@
 '''
 PDX Code Guild
 Class CodeBusters
-Week 02
+Week 04
 Lab 07 - Peaks and Valleys
 Jared Nistler
 '''
@@ -10,6 +10,17 @@ Jared Nistler
 # Instructions
 #-----------------------------------------------#
 '''
+Define the following functions:
+
+- peaks(data) - Returns the indices of peaks. A peak has a lower 
+number on both the left and the right.
+
+- valleys(data) - Returns the indices of 'valleys'. A valley is 
+a number with a higher number on both the left and the right.
+
+- peaks_and_valleys(data) - uses the above two functions to compile 
+a single list of the peaks and valleys in order of appearance in 
+the original data.
 
 '''
 #-----------------------------------------------#
@@ -29,3 +40,27 @@ Future Improvements:
 #-----------------------------------------------#
 # Import Dependencies
 #-----------------------------------------------#
+
+
+
+#-----------------------------------------------#
+# Define Functions
+#-----------------------------------------------#
+
+def peaks (data):
+    result_peaks = []
+    for i in range(len(data)):
+        if i == 0:
+            continue
+        elif data[i-1] < data[i] < data[i+1]:
+            result_peaks.append(data[i])
+    return result_peaks
+
+def valleys (data):
+
+    return
+
+def peaks_and_valleys (data):
+
+    return
+
