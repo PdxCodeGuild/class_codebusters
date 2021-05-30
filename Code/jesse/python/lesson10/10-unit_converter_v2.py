09db8dcab4ecdc5e1d348ca2634bd3acbb9e77fb
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-05-30 15:30:05 -0700

    updated lab 10 v2

--- Code/jesse/python/lesson10/10-unit_converter_v2.py
+++ Code/jesse/python/lesson10/10-unit_converter_v2.py
@@ -5,6 +5,12 @@ Allow the user to also enter the units. Then depending on the units, convert the
 1 mi is 1609.34 m
 1 m is 1 m
 1 km is 1000 m
+
+Below is some sample input/output:
+
+> what is the distance? 100
+> what are the units? mi
+> 100 mi is 160934 m
 '''
 
 units_in_meters = {
@@ -36,7 +42,7 @@ def check_amount(amount):
 measurement = check_measurement(input('Enter a measurement (ft, mi, m, or km): '))
 
 # get amount of the measurement from user
-amount = check_amount(input('Enter amount: '))
+amount = check_amount(input('Enter an amount: '))
 
 # convert measurment to meters
 in_meters = amount * units_in_meters[measurement]
