c121bcdf047fd0daf05689f355d3a1d0fe6164ab
J-Nistler <jmnistl@protonmail.com>
2021-06-25 08:59:43 -0400

    add features and functionality to phonebook app

--- Code/J-Nistler/django/phonebook/phonebook/urls.py
+++ Code/J-Nistler/django/phonebook/phonebook/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contacts/', include("contacts.urls"))
+    path('contacts/', include("contacts.urls")),
+    path('yellowpages/', include("yellowpages.urls")),
 ]
