d8897c6400dba17bdddd8bfceccd93d692c2de50
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-07-08 19:04:40 -0700

    lab 2 part 1 finished

--- Code/jesse/django/labs/lab2/views.py
+++ Code/jesse/django/labs/lab2/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.http import HttpResponse
 
 # Create your views here.
 
 def register(request):
-    return HttpResponse('hello world!')
+    if request.method == 'GET':
+        return render(request, 'lab2/register.html')
+    elif request.method == 'POST':
+        return HttpResponseRedirect(reverse('blog:profile'))
 
 def login(request):
-    return HttpResponse('hello world!')
+    if request.method == 'GET':
+        return render(request, 'lab2/login.html')
+    elif request.method == 'POST':
+        return HttpResponseRedirect(reverse('blog:profile'))
 
+@login_required
 def profile(request):
-    return HttpResponse('hello world!')
+    return render(request, 'lab2/profile.html')
