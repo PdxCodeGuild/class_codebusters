20400d7b8cbe0ee76e6aa516f9dcccf40f93a554
Jorge Sanmarti <techdevjorge@jorges-mbp.lan>
2021-07-09 18:34:51 -0700

    adding changes

--- Code/jorge/django/phonebook/phonebook/settings.py
+++ Code/jorge/django/phonebook/phonebook/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contacts',
-    'yellowpages'
+    'yellowpages',
+    'todo'
 ]
 
 MIDDLEWARE = [
--- Code/jorge/django/phonebook/phonebook/urls.py
+++ Code/jorge/django/phonebook/phonebook/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', include('contacts.urls')),
-    path('yellowpages/', include('yellowpages.urls'))
+    path('yellowpages/', include('yellowpages.urls')),
+    path('todo/', include('todo.urls'))
 ]
