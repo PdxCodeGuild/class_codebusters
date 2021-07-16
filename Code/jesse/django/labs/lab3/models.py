15c7b5b59ad6ee8bb59647bd9cf045bb7efccb9a
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-07-15 21:14:46 -0700

    added lab 3 - part 2

--- Code/jesse/django/labs/lab3/models.py
+++ Code/jesse/django/labs/lab3/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class PokemonType(models.Model):
     name =  models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Pokemon(models.Model):
     number = models.IntegerField()
     name = models.CharField(max_length=20)
     height = models.FloatField()
     weight = models.FloatField()
-    image_front = models.CharField(max_length=50)
-    image_back = models.CharField(max_length=50)
-    types = models.ManyToManyField(PokemonType)
+    image_front = models.CharField(max_length=100)
+    image_back = models.CharField(max_length=100)
+    types = models.ManyToManyField(PokemonType, related_name='pokemon')
+
+    def __str__(self):
+        return self.name
